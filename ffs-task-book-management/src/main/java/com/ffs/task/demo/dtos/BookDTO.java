045ae8dd8a72bd8b2c5c3cada8748b5304629c3c
hussein <husseinalrehany88@gmail.com>
2024-10-10 15:31:16 +0200

    creating DTOS

---
+++ ffs-task-book-management/src/main/java/com/ffs/task/demo/dtos/BookDTO.java
@@ -0,0 +1,16 @@
+package com.ffs.task.demo.dtos;
+
+import com.ffs.task.demo.entities.Type;
+import lombok.Data;
+
+@Data
+public class BookDTO {
+
+    private int id;
+
+    private String name;
+
+    private Type type;
+
+    private int authorId;
+}
